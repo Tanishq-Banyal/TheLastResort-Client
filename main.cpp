07980dac8589f4d47e33b07f9cf374a733eed3bb
Tanishq-Banyal <banyaltanishq@gmail.com>
2021-09-30 12:26:50 +0530

    initial

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include <SFML/Audio.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/Graphics.hpp>
+
+using std::cout, std::endl;
+
+int main(int argc, char* argv[])
+{
+	cout << "Test" << endl;
+	return 0;
+}
